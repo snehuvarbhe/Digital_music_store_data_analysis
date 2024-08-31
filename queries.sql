4c552adf9647b844ce193ea3822871cc44a7bc3c
Snehal Varbhe <96074682+snehuvarbhe@users.noreply.github.com>
2024-08-31 14:49:39 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,78 +1,83 @@
 /*	Question Set 1 - Easy */
 
 /* Q1: Who is the senior most employee based on job title? */
-SELECT title, last_name, first_name 
-FROM employee
-ORDER BY levels DESC
-LIMIT 1
+select * from employee
+order by levels desc
+limit 1;
 
 /* Q2: Which countries have the most Invoices? */
-SELECT COUNT(*) AS c, billing_country 
-FROM invoice
-GROUP BY billing_country
-ORDER BY c DESC
+select billing_country,count(1) as invoice_count from invoice
+group by billing_country
+order by count(1) desc
+limit 1;
 
 /* Q3: What are top 3 values of total invoice? */
-SELECT total 
-FROM invoice
-ORDER BY total DESC
+select * from invoice
+order by total desc
+limit 3;
 
 /* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
 Write a query that returns one city that has the highest sum of invoice totals. 
 Return both the city name & sum of all invoice totals */
-SELECT billing_city,SUM(total) AS InvoiceTotal
-FROM invoice
-GROUP BY billing_city
-ORDER BY InvoiceTotal DESC
-LIMIT 1;
+select billing_city,sum(total) as invoice_total from invoice
+group by billing_city
+order by sum(total) desc
+limit 1;
 
 /* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
 Write a query that returns the person who has spent the most money.*/
-SELECT customer.customer_id, first_name, last_name, SUM(total) AS total_spending
-FROM customer
-JOIN invoice ON customer.customer_id = invoice.customer_id
-GROUP BY customer.customer_id
-ORDER BY total_spending DESC
-LIMIT 1;
+select c.customer_id,c.first_name,c.last_name,sum(i.total) from customer c
+left join invoice i
+on c.customer_id=i.customer_id
+group by c.customer_id,c.first_name,c.last_name
+order by sum(i.total) desc
+limit 1;
 
 
 /* Question Set 2 - Moderate */
 
 /* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
 Return your list ordered alphabetically by email starting with A. */
-SELECT DISTINCT email,first_name, last_name
-FROM customer
-JOIN invoice ON customer.customer_id = invoice.customer_id
-JOIN invoiceline ON invoice.invoice_id = invoiceline.invoice_id
-WHERE track_id IN(
-	SELECT track_id FROM track
-	JOIN genre ON track.genre_id = genre.genre_id
-	WHERE genre.name LIKE 'Rock'
-)
-ORDER BY email;
+select distinct c.first_name,c.last_name,c.email
+from customer c join invoice i
+on c.customer_id=i.customer_id
+join invoice_line l
+on l.invoice_id=i.invoice_id
+join track t
+on t.track_id=l.track_id
+where t.track_id in (
+	select t.track_id
+	from track t join genre g
+	on t.genre_id=g.genre_id
+	where g.name like 'Rock')
+order by c.email;
 
 /* Q2: Let's invite the artists who have written the most rock music in our dataset. 
 Write a query that returns the Artist name and total track count of the top 10 rock bands. */
 
-SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
-FROM track
-JOIN album ON album.album_id = track.album_id
-JOIN artist ON artist.artist_id = album.artist_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
-GROUP BY artist.artist_id
-ORDER BY number_of_songs DESC
-LIMIT 10;
+select a1.artist_id, a1.name, count(a1.name) as Total_track_count from
+artist a1 join album a2
+on a1.artist_id=a2.artist_id
+join track t
+on t.album_id=a2.album_id
+where t.track_id in (
+	select t.track_id
+	from track t join genre g 
+	on t.genre_id=g.genre_id
+	where g.name like 'Rock'
+)
+group by a1.artist_id,a1.name
+order by count(a1.name) desc
+limit 10;
 
 
 /* Q3: Return all the track names that have a song length longer than the average song length. 
 Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */
-SELECT name,miliseconds
-FROM track
-WHERE miliseconds > (
-	SELECT AVG(miliseconds) AS avg_track_length
-	FROM track )
-ORDER BY miliseconds DESC;
+select name,milliseconds from track
+where milliseconds>(
+select avg(milliseconds) from track
+)
+order by milliseconds desc;
 
 
 /* Question Set 3 - Advance */
